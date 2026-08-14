0703215486e2182ae8c617f34c8dd1ebe868fb97
abu-npk <npk.abuthahir@gmail.com>
2026-08-14 16:22:38 +0530

    feat: integrate configuration with driver factory

--- src/main/java/com/banking/automation/driver/DriverFactory.java
+++ src/main/java/com/banking/automation/driver/DriverFactory.java
@@ -4,31 +4,47 @@ import java.util.Objects;
 
 import org.openqa.selenium.WebDriver;
 
+import com.banking.automation.config.ConfigurationManager;
 import com.banking.automation.enums.BrowserType;
 import com.banking.automation.factory.BrowserFactory;
 
-public class DriverFactory {
+public final class DriverFactory {
+	
 	private static final ThreadLocal<WebDriver> DRIVER = new ThreadLocal<>();
+	
+	private static final ConfigurationManager CONFIGURATION_MANAGER = new ConfigurationManager();
 	
 	private DriverFactory() {
 		throw new UnsupportedOperationException("DriverFactory should not be instantiated...");
 	}
 	
+	public static void initializeDriver() {
+		initializeDriver(CONFIGURATION_MANAGER.getBrowser());
+	}
+	
 	public static void initializeDriver(final BrowserType browserType) {
-		if(Objects.nonNull(DRIVER.get())){
+		if(Objects.isNull(browserType)){
 			throw new IllegalStateException(
-					"WebDriver is already initialized for the current thread...");
+					"Browser type cannot be null");
 		}
+		
+		if (Objects.nonNull(DRIVER.get())) {
+            throw new IllegalStateException(
+                    "WebDriver is already initialized for the current thread."
+            );
+        }
 		
 		final WebDriver webDriver = BrowserFactory.createDriver(browserType);
 		setDriver(webDriver);
 	}
 	
 	public static void setDriver(final WebDriver webDriver) {
+		
 		if(Objects.isNull(webDriver)) {
 			throw new IllegalArgumentException(
 					"WebDriver cannot be null...");
 		}
+		
 		DRIVER.set(webDriver);
 	}
 	
